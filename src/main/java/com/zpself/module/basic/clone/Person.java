de2386f46ef5d70f3946d2860b8dd6d804887f17
zengpeng <1094847554@qq.com>
2019-08-21 20:04:49 +0800

    提交：copy数组最的方法

---
+++ src/main/java/com/zpself/module/basic/clone/Person.java
@@ -0,0 +1,12 @@
+package com.zpself.module.basic;
+
+/**
+ * @author zengpeng
+ * @date 2019/8/21
+ */
+public class TestClone implements Cloneable{
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
---
+++ src/main/java/com/zpself/module/basic/clone/TestClone.java
@@ -0,0 +1,8 @@
+package com.zpself.module.basic;
+
+/**
+ * @author zengpeng
+ * @date 2019/8/21
+ */
+public class clone {
+}
