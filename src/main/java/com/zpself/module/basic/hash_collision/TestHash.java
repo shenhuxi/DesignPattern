09c55e94a1a339ccf3e753a3d925125467a210bc
zengpeng <1094847554@qq.com>
2019-08-22 10:26:23 +0800

    hashMap碰撞：当两个不同的键却有相同的hashCode时，他们会存储在同一个bucket位置的链表中。再利用键对象的equals()来找到键值对。

--- src/main/java/com/zpself/module/basic/hash_collision/TestHash.java
+++ src/main/java/com/zpself/module/basic/hash_collision/TestHash.java
@@ -1,8 +1,16 @@
 package com.zpself.module.basic.hash_collision;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author zengpeng
  * @date 2019/8/21
  */
 public class TestHash {
+    public static void main(String[] args) {
+        HashMap map = new HashMap(10);
+        map.put("1","2");
+        System.out.println(map);
+    }
 }
