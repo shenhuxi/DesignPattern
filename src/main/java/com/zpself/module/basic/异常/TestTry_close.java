1def465f760f9b9bcbcc6c6570f88599a08f0eba
zengpeng <1094847554@qq.com>
2019-09-02 17:37:00 +0800

    提交： 1.集合遍历不能改， 2.数组转换的集合不能加， 3.try()自带调用close(), 4.简单的序列化

---
+++ src/main/java/com/zpself/module/basic/异常/TestTry_close.java
@@ -0,0 +1,8 @@
+package com.zpself.module.basic.异常;
+
+/**
+ * @author zengpeng
+ * @date 2019/9/2
+ */
+public class TestTry_close {
+}
---
+++ src/main/java/com/zpself/module/basic/集合/ArrayChangeCollection.java
@@ -0,0 +1,8 @@
+package com.zpself.module.basic.集合;
+
+/**
+ * @author zengpeng
+ * @date 2019/9/2
+ */
+public class ArrayChangeCollection {
+}
